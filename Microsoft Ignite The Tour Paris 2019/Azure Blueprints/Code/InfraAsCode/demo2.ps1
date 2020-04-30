061cac77aae21f80cdaf7c94a34138d4bd436e0c
Yoann GUILLO <yoann.guillo@geneziis.com>
2020-04-30 20:48:56 +0200

    Last commit

--- Microsoft Ignite The Tour Paris 2019/Azure Blueprints/Code/InfraAsCode/demo2.ps1
+++ Microsoft Ignite The Tour Paris 2019/Azure Blueprints/Code/InfraAsCode/demo2.ps1
@@ -5,4 +5,4 @@ $CreatedDate = (Get-Date).ToUniversalTime().ToString("yyyyMMddTHHmmssZ")
 # Create a deployment name (to be unique)
 $DeploymentName = $CreatedBy.Split("@")[0] + "_" + $CreatedDate
 # Deploy the ARM template to a subscription level
-New-AzDeployment -Name $DeploymentName -Location westeurope -TemplateUri "https://github.com/YoannGUILLO/Community/blob/master/Microsoft Ignite The Tour Paris 2019/Azure Blueprints/Code/InfraAsCode/azuredeploy.json" -createdBy $CreatedBy
+New-AzDeployment -Name $DeploymentName -Location westeurope -TemplateUri "https://raw.githubusercontent.com/YoannGUILLO/Community/master/Microsoft%20Ignite%20The%20Tour%20Paris%202019/Azure%20Blueprints/Code/InfraAsCode/azuredeploy.json" -createdBy $CreatedBy
